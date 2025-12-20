38fe373745f5d84019a8c75a5ff61ce283c5bf12
haitongxu <xht325@gmail.com>
2025-12-20 01:10:57 +0000

    Deploying to gh-pages from @ haitongxu/haitongxu.github.io@d10a23afc7ffa156b77e2088f5443736e611a256 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,7 +35,7 @@ ninja.data = [{
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/teaching/";
+            window.location.href = "/services/";
           },
         },{id: "nav-recognitions",
           title: "Recognitions",
